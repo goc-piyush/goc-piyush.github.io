9c36e936823990b823d9a2f289c29c2e78d8eead
goc-piyush <80900612+goc-piyush@users.noreply.github.com>
2021-08-19 15:35:53 +0530

    Update mian.js

--- mian.js
+++ mian.js
@@ -1,2 +1,9 @@
 console.log('console log from github file');
 this.callLwc();
+window.postMessage(
+            {
+                message: "submited",
+                type: "chasitor.sendMessage"
+            },
+            window.parent.location.href
+    );
