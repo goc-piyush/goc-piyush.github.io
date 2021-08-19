6916cdfaa19a9c7e60ebdf6d5a741e42e0fba2b5
goc-piyush <80900612+goc-piyush@users.noreply.github.com>
2021-08-19 16:25:42 +0530

    Update mian.js

--- mian.js
+++ mian.js
@@ -7,3 +7,6 @@ window.postMessage(
             },
            window.parent.location.href
     );
+var myCustomData = { foo: 'submited' };
+var event = new CustomEvent('myEvent', { detail: myCustomData });
+window.parent.document.dispatchEvent(event)
