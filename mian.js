9bf25d26e03ce092b9427b8e09638d5168ee2e70
goc-piyush <80900612+goc-piyush@users.noreply.github.com>
2021-09-02 16:05:07 +0530

    Update mian.js

--- mian.js
+++ mian.js
@@ -1,12 +1,13 @@
 console.log('console log from github file');
-
-window.postMessage(
-            {
-                message: "submited",
-                type: "chasitor.sendMessage"
-            },
-           window.parent.location.href
-    );
 var myCustomData = { foo: 'submited' };
 var event = new CustomEvent('myEvent', { detail: myCustomData });
-window.parent.document.dispatchEvent(event)
+window.parent.document.dispatchEvent(event);
+
+// window.postMessage(
+//             {
+//                 message: "submited",
+//                 type: "chasitor.sendMessage"
+//             },
+//            window.parent.location.href
+//     );
+
